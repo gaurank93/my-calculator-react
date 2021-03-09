f9130ba906f8990b3a9c1cf158f201d88e1561fa
Gaurank Sharma <gourank93@gmail.com>
2021-03-09 15:38:11 +0530

    added button in index file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div><h1>My Calculator</h1></div>
+      <div>
+      <input type="text" readOnly/>
+      </div> 
+      <div >
+      <input type="text" readOnly/> 
+      </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
+      <div>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
       </header>
     </div>
   );
